8eaa7f275810767d248881f7cf6258373fa62f41
tsrana <tsrana@gmail.com>
2019-06-04 13:12:57 +0530

    4/6/19 1:12pm

--- run_build.sh
+++ run_build.sh
@@ -5,7 +5,11 @@ if [ "$PS" != "" ]
 then
 	echo "Stoping container --"
 	sudo docker stop "$PS"
+	echo "Removing Previous Image --"
+	sudo docker rmi $(sudo docker images s2-movie-info-service -q)
 fi
+echo "Creating Image..."
+sudo docker build -t s2-movie-info-service .
 echo "Staring container..."	
 sudo docker run -it -d -p 8082:8082 s2-movie-info-service	
 echo "End"
