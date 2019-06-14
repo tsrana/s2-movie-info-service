c6eb0c43a413ec34786f4c5925ba46fc74be3be8
tsrana <tsrana@gmail.com>
2019-06-15 00:24:04 +0530

    1224

--- run_build.sh
+++ run_build.sh
@@ -1,9 +1,10 @@
 echo "Start"
 IMAGE_NAME="tsrana1/s2-movie-info-service:${BUILD_NUMBER}"
 echo "Creating Image..."
-docker build -t $IMAGE_NAME .
-docker login -u tsrana1 -p${DOCKER_HUB}
-docker push $IMAGE_NAME
+echo "IMAGE_NAME--- $IMAGE_NAME"
+sudo docker build -t $IMAGE_NAME .
+sudo docker login -u tsrana1 -p${DOCKER_HUB}
+sudo docker push $IMAGE_NAME
 echo "Deploying Image to Kubernetes..."
 export KUBECONFIG=/home/ubuntu/kubeconfig/kubeConfig/kube-config-mil01-rana-cluster.yml
 kubectl set image deployment.v1beta1.extensions/s2-movie-info-service-deployment s2-movie-info-service=$IMAGE_NAME
